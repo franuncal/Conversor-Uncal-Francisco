b3fa9ff06a777b95162f31ebe9029c0fe6f4b639
luigi-coder <luigicoder28@gmail.com>
2023-12-11 17:28:07 -0300

    Modificacion entrega 1

--- Js/conversor.js
+++ Js/conversor.js
@@ -1,8 +1,8 @@
 // Definir tasas de cambio en array
 const tasasDeCambio = [
-  { divisa: "USD", valor: 1 }, //Dolar estadounidenese
-  { divisa: "EUR", valor: 0.85 }, //Euro
-  { divisa: "JPY", valor: 114.26 }, //Yen japones
+  { divisa: "USD", valor: 1 }, //Dolar estadounidenese 0
+  { divisa: "EUR", valor: 0.85 }, //Euro 1
+  { divisa: "JPY", valor: 114.26 }, //Yen japones 2
   { divisa: "ARS", valor: 99.5 }, //Peso Argentino
 ];
 
@@ -14,61 +14,85 @@ function saludarUsuario() {
 saludarUsuario();
 
 //Solicitar datos de usuario\
-const cantidad = parseFloat(prompt("Ingrese la cantidad a convertir: "));
-const monedaOrigen = prompt(
-  "Ingrese la moneda de ORIGEN (USD, EUR, JPY o ARS)");
-const monedaDestino = prompt("Ingrese la moneda de DESTINO (USD, EUR, JPY o ARS)");
+function datosUsuario() {
+  const cantidad = parseFloat(prompt("Ingrese la cantidad a convertir: "));
+  const monedaOrigen = prompt(
+    "Ingrese la moneda de ORIGEN (USD, EUR, JPY o ARS)");
+  const monedaDestino = prompt("Ingrese la moneda de DESTINO (USD, EUR, JPY o ARS)");
+
+  return {
+    cantidad: cantidad,
+    monedaOrigen: monedaOrigen,
+    monedaDestino: monedaDestino,
+  };
+}
 
 //Funcioin para convertir moneda
-function convertirMoneda(cantidad, monedaOrigen, monedaDestino) {
-  const tasaOrigen = tasasDeCambio[monedaOrigen];
-  const tasaDestino = tasasDeCambio[monedaDestino];
+function convertirMoneda(datosUsuario) {
 
-  if (tasaOrigen !== null && tasaDestino !== null) {
-    const cantidadConvertida = cantidad * (tasaDestino / tasaOrigen);
-    return cantidadConvertida;
+  const cantidad = datosUsuario.cantidad;
+  const monedaOrigen = datosUsuario.monedaOrigen;
+  const monedaDestino = datosUsuario.monedaDestino;
+
+  // validar con while cantidad
+  while (isNaN(cantidad)) {
+    alert("La cantidad ingresada no es valida.");
+    cantidad = parseFloat(prompt("Ingrese la cantidad a convertir: "));
+  }
+
+  const tasaOrigen = tasasDeCambio.find((tasa) => {
+    return tasa.divisa === monedaOrigen
+  });
+
+  const tasaDestino = tasasDeCambio.find((tasa) => {
+    return tasa.divisa === monedaDestino
+  });
+
+  if (tasaOrigen !== undefined && tasaDestino !== undefined) {
+
+    const cantidadConvertida = cantidad * (tasaDestino.valor / tasaOrigen.valor);
+    return {
+      cantidad: cantidad,
+      monedaOrigen: tasaOrigen.divisa,
+      monedaDestino: tasaDestino.divisa,
+      cantidadConvertida: cantidadConvertida,
+    };
+
   } else {
     alert("Moneda no valida.");
-  }
+  } 
 }
 
 //Realizar operacion
-const resultado = convertirMoneda(cantidad, monedaOrigen, monedaDestino);
+const resultado = convertirMoneda(datosUsuario());
 
-//Mostrar resultado por alert
-if (resultado) {
-  alert(
-    "El resultado de la operacion es: " +
-      cantidad +
-      monedaOrigen +
-      "\n equivale a\n " +
-      resultado +
-      monedaDestino
-  );
-}else(isNaN(resultado));{
-  alert("La operacion no puede realizarse.")
-}
+alert(
+  "El resultado de la operacion es: " +
+  resultado.cantidad +
+  resultado.monedaOrigen +
+  "\n equivale a \n " +
+  resultado.cantidadConvertida +
+  resultado.monedaDestino
+);
+ 
 
 //Consultar si desea realizar otra operacion
 const nuevaOperacion = prompt(
   "Desea realizar otra operacion? \n 1. Si \n 2. No"
 );
 if (nuevaOperacion === "1") {
-  cantidad = parseFloat(prompt("Ingrese la cantidad a convertir"));
 
-  monedaOrigen = prompt("Ingrese la moneda de ORIGEN (USD, EUR, JPY o ARS)");
-  monedaDestino = prompt("Ingrese la moneda de DESTINO (USD, EUR, JPY o ARS)");
-  resultado = convertirMoneda(cantidad, monedaOrigen, monedaDestino);
+  const resultado = convertirMoneda(datosUsuario());
 
   alert(
     "El resultado de la operacion es: " +
-      cantidad +
-      monedaOrigen +
-      "\n equivale a \n " +
-      resultado +
-      monedaDestino
+    resultado.cantidad +
+    resultado.monedaOrigen +
+    "\n equivale a \n " +
+    resultado.cantidadConvertida +
+    resultado.monedaDestino
   );
-} else nuevaOperacion === "2";
-{
+
+} else {
   alert("Muchas gracias por utilizar nuestro conversor de divisas.");
 }
